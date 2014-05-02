e1cccc7b268d4b6bd2d0725bf2bb31d57eea93e5
Andrew Greenhill <andrewgggg@iinet.net.au>
2014-05-02 22:15:59 +0800

    End 9.3.1 add a basic users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
   def show
   	@user = User.find(params[:id])
   end
@@ -19,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       #Handle a successful update:
       flash[:success] = "Profile updated"
@@ -33,6 +38,20 @@ class UsersController < ApplicationController
       #Handle failed update attempt:
       render 'edit'
     end
+  end 
+
+  private
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 
 end
